3bc91c0f134c479ccdb87c773ea942298729696a
Mafuyeka <tshepisoroshina@gmail.com>
2025-06-03 05:49:18 -0700

    Updated

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,12 +12,12 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f'"{item}" has been added to the list.\n')
 
         elif choice == '2':
-            item = input("Enter item to remove: ")
+            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f'"{item}" has been removed from the list.\n')
@@ -42,3 +42,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
